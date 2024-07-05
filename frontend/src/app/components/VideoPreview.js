d8363876c4a76402a897c6f76b29165afae7a716
cleavecamael <phoenixlee125@gmail.com>
2024-07-06 01:12:47 +0800

    Allowed Resizing for Portrait Videos

--- frontend/src/app/components/VideoPreview.js
+++ frontend/src/app/components/VideoPreview.js
@@ -1,27 +1,27 @@
 'use client'
-import { React, Suspense, useState, useEffect} from "react";
+import { React, Suspense, useState, useEffect, useRef} from "react";
 import Loading from "./Loading";
 
 export default function VideoPreview({videoFile}){
-    const [widthName, setWidth] = useState("");
 
+    var [widthName, setWidth] = useState()
     var video = document.createElement('video');
     var src = URL.createObjectURL(videoFile);
-    
+    video.src = src
+    video.onloadedmetadata = () => {
+        console.log("done")
+        var width = video.videoWidth;
+        var height = video.videoHeight;
+        setWidth((width >= height) ? "w-[90%]" : "h-[80%]")
+    };
    
    
         
-        return(
-            <video onLoad={(response) => {
-                
-                const { width, height } = response.naturalSize;
-                setWidth((width >= height) ? "w-[90%]" : "w-[10%]")
-                
-                setWidth("cock")
-            }}
-            className={widthName} controls>
-            <source src={URL.createObjectURL(videoFile)}/>
-            </video> 
-        )
+    return(
+        <video
+        className={widthName} controls>
+        <source src={URL.createObjectURL(videoFile)}/>
+        </video> 
+    )
     
 }
