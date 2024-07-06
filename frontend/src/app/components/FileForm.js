832b18736b23b506985f6a4dc1274146a0b47eda
cleavecamael <phoenixlee125@gmail.com>
2024-07-06 20:18:29 +0800

    Fixed Overflow issues

--- frontend/src/app/components/FileForm.js
+++ frontend/src/app/components/FileForm.js
@@ -31,15 +31,15 @@ export default function FileForm(){
         alert("Select Translation Language")
     }
     }
-    if (resultState == "start"){
+    if (resultState == "start"){    
         return (
     
-            <div className={` transition-all duration-300 ${(fileState == "active" ? "h-[700px] w-[900px] p-5 pt-10" : "h-[500px] w-[700px] p-10")} bg-lightgrey border-black border-2 drop-shadow-xl rounded-[18px] flex flex-col items-center justify-between`}>
+            <div className={`mr-10 transition-all duration-300 ${(fileState == "active" ? "h-[500px] w-[750px] p-5 pt-10" : "h-[450px] w-[600px] p-10")} bg-lightgrey border-black border-2 drop-shadow-xl rounded-[18px] flex flex-col items-center justify-between`}>
                 <h1 className='font-medium text-4xl w-[90%] mb-2'>Translate your Content</h1>
                 <Uploadfile setFile = {setFile}/>
                 <div className="w-[80%] flex flex-row items-center justify-end">
                     <FormDrop setDub = {setDub}/>
-                    <SmallButton onClick={sendFile} text="Dub It!" className="ml-10 bg-purple hover:bg-darkpurple drop-shadow-lg"/>
+                    <SmallButton onClick={sendFile} text="Dub It!" className="ml-10 bg-darkpurple hover:bg-purple drop-shadow-lg"/>
                 </div>
                 
             </div>
