7b107f59d3da68a0cab7e5e7a9c8f7db28b2c155
cleavecamael <phoenixlee125@gmail.com>
2024-07-06 20:21:57 +0800

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -7,8 +7,8 @@ import { Main } from "next/document";
 import Image from 'next/image'
 export default function Home() {
   return (
-    <main className="h-[100vh] flex flex-row items-center justify-center">
-      <MainDescription/><Image src="/prototype.png"  width= {276 * 2} height={448 * 2} />
+    <main className="relative overflow-hidden h-[90vh] flex flex-row items-center justify-center">
+      <MainDescription/><Image src="/prototype.png"  width= {276 * 1.5} height={448 * 1.5} />
     </main>
   );
 }
