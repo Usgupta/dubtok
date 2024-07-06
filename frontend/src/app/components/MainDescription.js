217f7431eb5072b5472b4939bcd210eb8a04d823
cleavecamael <phoenixlee125@gmail.com>
2024-07-06 23:23:22 +0800

    Fixed Responsiveness for landing page

--- frontend/src/app/components/MainDescription.js
+++ frontend/src/app/components/MainDescription.js
@@ -3,12 +3,12 @@ import Button from "./Button";
 import Link from 'next/link'
 export default function MainDescription({className}){
     return (
-        <div id="mainDescription" className={`w-[200px] lg:w-[705px] h-[40vh] p-[20px]  flex flex-col justify-between mr-6 ${className}`}>
-            <h1 className="font-bold text-xl lg:text-6xl">Diversify your content</h1>
-            <p className=" text-sm lg:text-2xl">Dubtok empowers creators to reach a global audience by allowing their videos to be viewed in different languages</p>
+        <div id="mainDescription" className={`w-[200px] md:w-[600px] lg:w-[705px] h-[300px] p-[20px]  flex flex-col justify-between mr-6 ${className}`}>
+            <h1 className="font-bold text-xl md: text-4xl lg:text-6xl">Diversify your content</h1>
+            <p className=" text-sm md:text-xl lg:text-2xl">Dubtok empowers creators to reach a global audience by allowing their videos to be viewed in different languages</p>
         
-        < div id="buttons" className="w-[200px] md:w-[400px] flex flex-col lg:flex-row justify-between">
-            <Link href="/demo"><Button className="bg-red hover:bg-darkred my-2  lg:mr-2 lg:my-0" text="Get Started"/></Link>
+        < div id="buttons" className="w-[200px] md:w-[400px] flex flex-col md:flex-row justify-between">
+            <Link href="/demo"><Button className="bg-red hover:bg-darkred my-2  md:mr-2 md:my-0" text="Get Started"/></Link>
             <Button className="bg-black hover:bg-lightblack" text="Learn More"/>
         </div>
         </div>
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -7,7 +7,7 @@ import { Main } from "next/document";
 import Image from 'next/image'
 export default function Home() {
   return (
-    <main className="relative overflow-hidden h-[90vh] flex flex-row lg:items-center justify-center">
+    <main className="relative overflow-hidden h-[90vh] flex flex-row mt-20 lg:mt:0 lg:items-center justify-center">
       <MainDescription className="mt-20 md:mt-0"/>
       <div className='w-[200px] lg:w-[276px] mt-20 md:mt-0'>
       <Image  src="/prototype.png" width= {276} height={448} />
