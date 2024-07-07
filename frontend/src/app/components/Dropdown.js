73e16da92d002298f9ceeecbae501498331c1f05
cleavecamael <phoenixlee125@gmail.com>
2024-07-07 22:26:50 +0800

    Update Dropdown.js

--- frontend/src/app/components/Dropdown.js
+++ frontend/src/app/components/Dropdown.js
@@ -17,9 +17,7 @@ export default function FormDrop({setDub}) {
   } 
   return (
     <Dropdown  theme={{ floating: { target: "hover:bg-lightgrey bg-white px-0 py-0 text-xs text-black w-24 md:w-32"} }} label={selected} dismissOnClick={false}>
-      <Dropdown.Item onClick={() => setOption("English")}>English</Dropdown.Item>
       <Dropdown.Item onClick={() => setOption("French")}>French</Dropdown.Item>
-      <Dropdown.Item onClick={() => setOption("Chinese")}>Chinese</Dropdown.Item>
       <Dropdown.Item onClick={() => setOption("Malay")}>Malay</Dropdown.Item>
     </Dropdown> 
   );
