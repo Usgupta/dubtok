76892191d91e5b32f9011b5181f59274feed23a0
cleavecamael <phoenixlee125@gmail.com>
2024-07-07 23:57:04 +0800

    Added Link and Mobile Prototype

--- frontend/src/app/components/Navbar.js
+++ frontend/src/app/components/Navbar.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Image from 'next/image'
 import { IBM_Plex_Mono } from "next/font/google";
-
+import Link from 'next/link'
 export default function Navbar(){
     return(
         <nav>
             <div className="bg-black h-[10vh] rounded-xm flex flex-row items-center font-default font-bold">
-            <Image src="/logo-white2.png" className='pl-5' width= {2000 / 10} height={439 / 10} />
+            <Link href="/"> <Image src="/logo-white2.png" className='pl-5' width= {2000 / 10} height={439 / 10} /></Link>
             <p className="ml-auto text-3xl pr-10">About</p>
             </div>
         </nav>
-      ) 
-      
-}       
+      )
+
+}
