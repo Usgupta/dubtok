879f78f04a187a069320279e92c5a8726f4ea98c
cleavecamael <phoenixlee125@gmail.com>
2024-07-08 06:09:50 +0800

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
     <main className="relative overflow-hidden h-[90vh] flex flex-row lg:mt:0 lg:items-center justify-center">
       <MainDescription className="mt-20 md:mt-0"/>
       <div className='w-[200px] lg:w-[304px] mt-20 md:mt-0'>
-      <Image  src="/prototype.png" width= {276 * 1.1} height={448 * 1.1} />
+      <Image src="/Prototype.png" width= {276 * 1.1} height={448 * 1.1} alt="Prototype Image" />
       </div>
 
     </main>
