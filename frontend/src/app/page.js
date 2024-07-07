89dd93386a5f4c710711d7faf77c04bf866c3a05
cleavecamael <phoenixlee125@gmail.com>
2024-07-08 00:15:14 +0800

    Remove Import

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,17 +3,16 @@ import FileForm  from "./components/FileForm";
 import DragBox from "./components/DragBox";
 import Button from "./components/Button";
 import MainDescription from "./components/MainDescription";
-import { Main } from "next/document";
 import Image from 'next/image'
 import Head from "next/head";
 export default function Home() {
-  return (  
+  return (
     <main className="relative overflow-hidden h-[90vh] flex flex-row lg:mt:0 lg:items-center justify-center">
       <MainDescription className="mt-20 md:mt-0"/>
       <div className='w-[200px] lg:w-[304px] mt-20 md:mt-0'>
       <Image  src="/prototype.png" width= {276 * 1.1} height={448 * 1.1} />
       </div>
-      
+
     </main>
   );
 }
